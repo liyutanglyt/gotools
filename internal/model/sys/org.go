166bee6117989cd01fd032e1a546c689a349c539
wangwei <2531868871@qq.com>
2019-12-09 16:25:46 +0800

    delete

--- internal/model/sys/org.go
+++
@@ -1,8 +0,0 @@
-package sys
-
-type Org struct {
-	Id       int64  `json:"id"`
-	ParentId int64  `json:"parent_id"`
-	Name     string `json:"name"`
-	Code     string `json:"code"`
-}
--- templates/project/internal/conf/casbin_conf.go
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Wangwei on 2019-07-06 11:42.
- */
-
-package conf
-
-type CasbinConfig MySQLConfig
